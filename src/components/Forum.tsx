f6d3197b11e8b45fe2e3110caf5a70cf6cd10c21
Narendran M <narendranmurali2001@gmail.com>
2024-12-16 16:04:51 +0530

    Forum filtering

--- src/components/Forum.tsx
+++ src/components/Forum.tsx
@@ -14,68 +14,85 @@ const Forum: React.FC = () => {
     }
 
     const [feedbackList, setFeedbackList] = useState<Feedback[]>([]);
+    // const [searchQuery, setSearchQuery] = useState('');
+    const [displayList, setDisplayList] = useState<Feedback[]>(feedbackList);
+
     
     const fetchList = async () => {
         try {
             const response = await axios.get('http://localhost:3000/feedback/');
             setFeedbackList(response.data.reverse());
+            setDisplayList(response.data.reverse());
             // feedbackList.map((feedback) => {
-            //     const formattedDate = feedback.submittedAt.toLocaleDateString('en-US', { 
-            //         year: 'numeric', 
-            //         month: 'long', 
-            //         day: 'numeric' 
-            //       }); 
-            //       const formattedTime = feedback.submittedAt.toLocaleTimeString('en-US', { 
-            //         hour: '2-digit', 
-            //         minute: '2-digit' 
-            //       });
-            //     feedback.submittedAt = `${formattedDate} ${formattedTime}`;
-            // })
-            console.log('List fetch successfull: ', feedbackList);
-        } catch (error) {
-            console.error('Error fetching feedbacks:', error);
-        };
-    }
+                //     const formattedDate = feedback.submittedAt.toLocaleDateString('en-US', { 
+                    //         year: 'numeric', 
+                    //         month: 'long', 
+                    //         day: 'numeric' 
+                    //       }); 
+                    //       const formattedTime = feedback.submittedAt.toLocaleTimeString('en-US', { 
+                        //         hour: '2-digit', 
+                        //         minute: '2-digit' 
+                        //       });
+                        //     feedback.submittedAt = `${formattedDate} ${formattedTime}`;
+                        // })
+                        console.log('List fetch successfull: ', feedbackList);
+                    } catch (error) {
+                        console.error('Error fetching feedbacks:', error);
+                    };
+                }
+                
 
     useEffect(() => {
         fetchList();
     }, []);
 
-    return (
-        <div id="forum">
-            <h1>Feedback Forum</h1>
-            <div className="filters">
-                
-            </div>
-            <ul>
-                {feedbackList.map((feedback) => (
-                    <li key={feedback._id}>
-                        {/* <p><strong>Employee ID:</strong> {feedback.employee_id || "Anonymous"}</p>
-                        <p><strong>Feedback:</strong> {feedback.feedbackText}</p>
-                        <p><strong>Rating:</strong> {feedback.rating}</p> */}
-                    
-                        <div className="card">
-                            <div className="author">
-                                <strong>Review for</strong> {feedback.employee_id || "Anonymous"}
-                            </div>
 
-                            <div className="infos">
-                                <p className="date-time">
-                                    {String(feedback.submittedAt)}
-                                </p>
-                                <p className="description">
-                                    {feedback.feedbackText}
-                                </p>
+
+        return (
+            <div id="forum">
+                <div className="title">
+                    <h1>Feedback Forum</h1>
+                    <div className="filters">
+                        <label htmlFor="filter">Search:</label>
+                        <input type="text" placeholder="Id here..." id="filter" onChange={(e) => {
+                            { if (e.target.value !== '')
+                                setDisplayList(feedbackList.filter((feedback) => feedback.employee_id.includes(e.target.value)));
+                                else setDisplayList(feedbackList);
+                            }
+                        }}/>
+                    </div>
+                </div>
+                 { displayList.length > 0 ? (
+                <ul>
+                    {displayList.map((feedback) => (
+                        <li key={feedback._id}>
+                            {/* <p><strong>Employee ID:</strong> {feedback.employee_id || "Anonymous"}</p>
+                            <p><strong>Feedback:</strong> {feedback.feedbackText}</p>
+                            <p><strong>Rating:</strong> {feedback.rating}</p> */}
+                        
+                            <div className="card">
+                                <div className="author">
+                                    <strong>Review for</strong> {feedback.employee_id}
+                                </div>
+    
+                                <div className="infos">
+                                    <p className="date-time">
+                                        {String(feedback.submittedAt)}
+                                    </p>
+                                    <p className="description">
+                                        {feedback.feedbackText}
+                                    </p>
+                                </div>
+                                <div className="stars">
+                                    <StarRating rating={feedback.rating} />
+                                </div>
                             </div>
-                            <div className="stars">
-                                <StarRating rating={feedback.rating} />
-                            </div>
-                        </div>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
-}
+                        </li>
+                    ))}
+                </ul>
+                ) : ( <p>No feedbacks found.</p> )}
+            </div>
+        )
+    }
 
 export default Forum;
